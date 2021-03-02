460091dafaf0d3b585c128ad8588e9226752f2bd
Lucas Santos <lucassantos@ciandt.com>
2021-03-02 12:01:19 -0300

    using REQUEST_METHOD

---
+++ Exercicios/Ex006.php
@@ -0,0 +1,15 @@
+<?php
+
+function findDay($choosenDay) 
+{
+
+$days = array(
+    'domingo' => '1',
+    'segunda'  => '2',
+    'terça' => '3',
+    'quarta' => '4',
+    'quinta' => '5',
+    'sexta' => '6',
+    'sabado' => '7'
+);
+}
--- Exercicios/addAPet.php
+++ Exercicios/addAPet.php
@@ -3,12 +3,62 @@
 require './Ex002.php';
 include './CPFValidator.php';
 ?>
+<?php
+
+if ($_SERVER = ['REQUEST_METHOD'] == 'POST') {
 
 
 
 
-Hi <?php echo htmlspecialchars($_POST['name']); ?>.
-You are <?php echo nl2br((int)$_POST['age']); ?> years old.
-The pet's breed is  <?php echo htmlspecialchars($_POST['breed']); ?>
- Pet's weight is  <?php echo htmlspecialchars($_POST['weight']); ?>
- Your <?php echo validatingCPF((int)$_POST['cpf']); ?>.
+
+    
+
+    // use ISSET as a shortly way to write array_key_exist() [VERIFICA SE A ARRAY EXISTE]
+
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+    } else {
+        $name = '';
+    }
+
+
+    if (isset($_POST['age'])) {
+        $age = $_POST['age'];
+    } else {
+        $age = '';
+    }
+
+
+
+    if (isset($_POST['breed'])) {
+        $breed = $_POST['breed'];
+    } else {
+        $breed = '';
+    }
+
+
+
+    if (isset($_POST['weight'])) {
+        $weight = $_POST['weight'];
+    } else {
+        $weight = '';
+    }
+
+
+
+    if (isset($_POST['cpf'])) {
+        $cpf = $_POST['cpf'];
+    } else {
+        $cpf = '';
+    }
+}
+
+?>
+
+
+
+Hi <?php echo htmlspecialchars($name); ?>.
+You are <?php echo (int)$age; ?> years old.
+The pet's breed is <?php echo htmlspecialchars($_POST['breed']); ?>
+Pet's weight is <?php echo htmlspecialchars($_POST['weight']); ?>
+Your  <?php echo validatingCPF((int)$_POST['cpf']); ?>.
