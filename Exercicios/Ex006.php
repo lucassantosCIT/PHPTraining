dd0f950d9fd387b49786f602095f8454dd0d8f8f
Lucas Santos <lucassantos@ciandt.com>
2021-03-02 13:58:52 -0300

    invalid day added ad ex06

--- Exercicios/Ex006.php
+++ Exercicios/Ex006.php
@@ -4,23 +4,27 @@ function findDay($choosenDay)
 {
 
     $days = array(
-    1 => 'domingo',
-    2 => 'segunda',
-    3 => 'terça',
-    4 => 'quarta',
-    5 => 'quinta',
-    6 => 'sexta',
-    7 =>' sabado'
+    1 => 'sunday',
+    2 => 'morning',
+    3 => 'thursday',
+    4 => 'wednesday',
+    5 => 'thursday',
+    6 => 'friday',
+    7 => 'saturday'
     );
+
+
+    if ($choosenDay>7) {
+        return "Invalid Day";
+    }
 
     foreach ($days as $listDays) {
         return $days[$choosenDay];
 
     }
 
-
 }
     // Parameter is the number of the day
-echo findDay(5);
+echo "The choosen day is " ,findDay(4);
 
 
