95213923ed4a93d45298cf21c577771b7c15b4af
Lucas Santos <lucassantos@ciandt.com>
2021-03-03 14:38:14 -0300

    bug CPF validator fixed

--- Exercicios/CPFValidator.php
+++ Exercicios/CPFValidator.php
@@ -1,23 +1,52 @@
 <?php
 
-function validatingCPF($cpf) {
+function validatingCPF($cpf) 
+{
  
-        // CREDITS FOR THE CPF FORMULA CODE: RAFAEL-NERI (GITHUB)
-    // Verify if there are a total of 11 numbers
+
+    // Verify if the fill is writed
+if (empty($cpf)) 
+     {
+        return "CPF is invalid";
+     }
+
+    // Elimina possivel mascara
+    $cpf = preg_replace("/[^0-9]/", "", $cpf);
+    $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+    // Verify if the total of numbers if 11
     if (strlen($cpf) != 11) {
-        return " CPF is invalid";
+        return "CPF is invalid";
     }
+    // Verify the sequences
+    else if ($cpf == '00000000000' || 
+         $cpf == '11111111111' || 
+         $cpf == '22222222222' || 
+         $cpf == '33333333333' || 
+         $cpf == '44444444444' || 
+         $cpf == '55555555555' || 
+         $cpf == '66666666666' || 
+         $cpf == '77777777777' || 
+         $cpf == '88888888888' || 
+         $cpf == '99999999999') {
+        return "CPF is invalid";
 
-    // Calculating CPF's validation
-    for ($t = 9; $t < 11; $t++) {
-        for ($d = 0, $c = 0; $c < $t; $c++) {
-            $d += $cpf[$c] * (($t + 1) - $c);
+
+        // CPF Digits Validator 
+    } else 
+    {   
+    
+        for ($t = 9; $t < 11; $t++) {
+        
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf{$c} * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf{$c} != $d) {
+                return "CPF is invalid";
+            }
         }
-        $d = ((10 * $d) % 11) % 10;
-        if ($cpf[$c] != $d) {
-            return "CPF in invalid";
-        }
+
+        return "CPF is valid"; 
     }
-    return " CPF is valid";
-
 }
